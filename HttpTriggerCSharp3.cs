ac2b77eda883f4fd6b0336f5197fd7aa36c9cba9
Kamil Stadryniak <kamil.stadryniak@ttpsc.pl>
2020-12-03 14:27:36 +0100

    some changes

--- HttpTriggerCSharp3.cs
+++ HttpTriggerCSharp3.cs
@@ -8,13 +8,13 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace Kstadryniak.MsLearn.Functions.VsCodeFunction2
+namespace Kstadryniak.MsLearn.Functions.VSCodeFunction3
 {
-    public static class HttpTriggerCSharp2
+    public static class HttpTriggerCSharp3
     {
-        [FunctionName("HttpTriggerCSharp2")]
+        [FunctionName("HttpTriggerCSharp3")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "in")] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -27,7 +27,7 @@ namespace Kstadryniak.MsLearn.Functions.VsCodeFunction2
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"HUehuehue Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
         }
